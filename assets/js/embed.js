c6f9fbfa75aea22599e3b470e96f493c31ab8a2e
RaioDigital <raiodigitalweb@gmail.com>
2022-08-17 14:39:19 -0300

    Alteraçãoes do menu e dos botoes e links da pagina Hub

---
+++ assets/js/embed.js
@@ -0,0 +1,14 @@
+function abreInstagran() {
+    var novaJanela = window.open("https://lp.sebraeceara.com.br/analise-do-instagram", "_blank","width=425 height=800 border-radius=12px", "position=static");
+}  
+
+function abreGoogleNeg() {
+    var novaJanela = window.open("https://lp.sebraeceara.com.br/google-meu-negocio", "_blank",'height=800, width=425, left='+(window.innerWidth-0)/2+', top='+(window.innerHeight-700)/2);
+}  
+
+function abreEmpretec() {
+    var novaJanela = window.open("https://lp.sebraeceara.com.br/lp-empretec", "_blank","width=425,height=800,left=" + (screen.width - 800) / 2 + ",top=" + (screen.innerHeight - 600) / 2);
+} 
+/*Alinhamento no centro da tela independente do tamanho.
+
+window.open("http://www.google.com", "", "width=800,height=600,left=" + (screen.width - 800) / 2 + ",top=" + (screen.height - 600) / 2);*/
