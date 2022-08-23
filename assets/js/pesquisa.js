b938215af8efbcd379850ef495b645fd1a0db31f
RaioDigital <raiodigitalweb@gmail.com>
2022-08-23 19:02:52 -0300

    Implementação do botão de pesquisa e mudanças com relação ao controle

--- assets/js/pesquisa.js
+++ assets/js/pesquisa.js
@@ -1,5 +1,29 @@
-function pesquisar(){
-    var section, div;
-    input = document.getElementById("#pesquisa"); 
-    filtro = input.value.toUpperCase();
+
+const INPUT_BUSCA = document.getElementById("pesquisa");
+/*const DOWNLOAD = document.getElementById('materiais');*/
+const SERVICES = document.getElementById("services");
+
+INPUT_BUSCA.addEventListener('keyup',()=>{
+   let expressao = INPUT_BUSCA.value.toLowerCase();
+
+   if (expressao.length === 1){
+    return;
+   }
+
+   let containerBox = document.querySelectorAll(".box");
+   
+   console.log(containerBox);
+
+   for(let posicao in containerBox){
+   if(isNaN(posicao)){
+        continue;
 }
+    let conteudoDoContainerBox = containerBox[posicao].innerHTML.toLowerCase();
+    if(true ===conteudoDoContainerBox.includes(expressao)){
+        containerBox[posicao].style.display='';
+    }else{
+        containerBox[posicao].style.display='none';
+    }
+   }
+
+})
