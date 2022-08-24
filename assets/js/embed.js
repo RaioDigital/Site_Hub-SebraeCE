8546095947021fdaad0771ff79fe9a2418b4d5a3
RaioDigital <raiodigitalweb@gmail.com>
2022-08-24 17:08:48 -0300

    Alteração de imagens diminuindo o peso das imagens das landpages

--- assets/js/embed.js
+++ assets/js/embed.js
@@ -1,13 +1,14 @@
+/*
 function abreInstagran() {
-    var novaJanela = window.open("https://lp.sebraeceara.com.br/analise-do-instagram", "_blank","width=425 height=800 border-radius=12px", "position=static");
+    var novaJanela = window.open("form.html", "_blank","width=425 height=800 border-radius=12px", "position=static");
 }  
 
 function abreGoogleNeg() {
     var novaJanela = window.open("https://lp.sebraeceara.com.br/google-meu-negocio", "_blank",'height=800, width=425, left='+(window.innerWidth-0)/2+', top='+(window.innerHeight-700)/2);
 }  
-
-function abreEmpretec() {
-    var novaJanela = window.open("https://lp.sebraeceara.com.br/lp-empretec", "_blank","width=425,height=800,left=" + (screen.width - 800) / 2 + ",top=" + (screen.innerHeight - 600) / 2);
+*/
+function download() {
+    var novaJanela = window.open("form.html", "_blank","width=425,height=800,left=" + (screen.width - 800) / 2 + ",top=" + (screen.innerHeight - 600) / 2);
 } 
 /*Alinhamento no centro da tela independente do tamanho.
 
