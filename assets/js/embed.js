27caa476903722c5103ecb882ccf8f37e1f18377
RaioDigital <raiodigitalweb@gmail.com>
2022-08-26 15:27:12 -0300

    Ajuste do botão download

--- assets/js/embed.js
+++ assets/js/embed.js
@@ -1,4 +1,4 @@
 
-function download() {
-    var novaJanela = window.open("form.html", "_blank","width=425,height=800,left=" + (screen.width - 800) / 2 + ",top=" + (screen.innerHeight - 600) / 2);
+function download1() {
+    var novaJanela = window.open("form1.html", "_blank","width=425,height=800,left=" + (screen.width - 800) / 2 + ",top=" + (screen.innerHeight - 600) / 2);
 } 
