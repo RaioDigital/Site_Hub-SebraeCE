037f8c21f8573b2dcb8a9914933453b461624cf4
RaioDigital <raiodigitalweb@gmail.com>
2022-08-26 14:20:12 -0300

    Ajustes das LPs

--- assets/js/embed.js
+++ assets/js/embed.js
@@ -1,15 +1,4 @@
-/*
-function abreInstagran() {
-    var novaJanela = window.open("form.html", "_blank","width=425 height=800 border-radius=12px", "position=static");
-}  
 
-function abreGoogleNeg() {
-    var novaJanela = window.open("https://lp.sebraeceara.com.br/google-meu-negocio", "_blank",'height=800, width=425, left='+(window.innerWidth-0)/2+', top='+(window.innerHeight-700)/2);
-}  
-*/
 function download() {
     var novaJanela = window.open("form.html", "_blank","width=425,height=800,left=" + (screen.width - 800) / 2 + ",top=" + (screen.innerHeight - 600) / 2);
 } 
-/*Alinhamento no centro da tela independente do tamanho.
-
-window.open("http://www.google.com", "", "width=800,height=600,left=" + (screen.width - 800) / 2 + ",top=" + (screen.height - 600) / 2);*/
