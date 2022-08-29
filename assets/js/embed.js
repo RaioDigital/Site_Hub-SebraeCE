b43defebcfa3e79165856083c274141e615c4007
RaioDigital <raiodigitalweb@gmail.com>
2022-08-29 18:59:49 -0300

    Criando o Slide

--- assets/js/embed.js
+++ assets/js/embed.js
@@ -1,16 +1,16 @@
 
 function download1() {
-    var novaJanela = window.open("form1.html", "_blank","width=425,height=800,left=" + (screen.width - 800) / 2 + ",top=" + (screen.innerHeight - 600) / 2);
+    var novaJanela = window.open("form1.html", "_blank","width=505,height=1100,left=" + (screen.width - 800) / 2 + ",top=" + (screen.innerHeight - 600) / 2);
 } 
 
 function download2() {
-    var novaJanela = window.open("form2.html", "_blank","width=425,height=800,left=" + (screen.width - 800) / 2 + ",top=" + (screen.innerHeight - 600) / 2);
+    var novaJanela = window.open("form2.html", "_blank","width=505,height=1100,left=" + (screen.width - 800) / 2 + ",top=" + (screen.innerHeight - 600) / 2);
 } 
 
 function download3() {
-    var novaJanela = window.open("form3.html", "_blank","width=425,height=800,left=" + (screen.width - 800) / 2 + ",top=" + (screen.innerHeight - 600) / 2);
+    var novaJanela = window.open("form3.html", "_blank","width=505,height=1100,left=" + (screen.width - 800) / 2 + ",top=" + (screen.innerHeight - 600) / 2);
 } 
 
 function download4() {
-    var novaJanela = window.open("form4.html", "_blank","width=425,height=800,left=" + (screen.width - 800) / 2 + ",top=" + (screen.innerHeight - 600) / 2);
+    var novaJanela = window.open("form4.html", "_blank","width=505,height=1100,left=" + (screen.width - 800) / 2 + ",top=" + (screen.innerHeight - 600) / 2);
 } 
