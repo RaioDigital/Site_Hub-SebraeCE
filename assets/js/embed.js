de3c294de4cb518f66017c696db4d7330780ce52
RaioDigital <raiodigitalweb@gmail.com>
2022-09-05 17:14:15 -0300

    Alteração do embed.js

--- assets/js/embed.js
+++ assets/js/embed.js
@@ -1,16 +1,16 @@
 
 function download1() {
-    var novaJanela = window.open("form1.html", "_blank","width=505,height=1100,left=" + (screen.width - 800) / 2 + ",top=" + (screen.innerHeight - 600) / 2);
+    var novaJanela = window.open("https://lp.sebraeceara.com.br/e-book-direitos-e-responsabilidades-serie-mei", "_blank","width=505,height=1100,left=" + (screen.width - 800) / 2 + ",top=" + (screen.innerHeight - 600) / 2);
 } 
 
 function download2() {
-    var novaJanela = window.open("form2.html", "_blank","width=505,height=1100,left=" + (screen.width - 800) / 2 + ",top=" + (screen.innerHeight - 600) / 2);
+    var novaJanela = window.open("https://lp.sebraeceara.com.br/inforgrafico-salao-parceiro-o-que-e-e-como-funciona", "_blank","width=505,height=1100,left=" + (screen.width - 800) / 2 + ",top=" + (screen.innerHeight - 600) / 2);
 } 
 
 function download3() {
-    var novaJanela = window.open("form3.html", "_blank","width=505,height=1100,left=" + (screen.width - 800) / 2 + ",top=" + (screen.innerHeight - 600) / 2);
+    var novaJanela = window.open("https://lp.sebraeceara.com.br/infografico-o-que-sao-canais-de-venda", "_blank","width=505,height=1100,left=" + (screen.width - 800) / 2 + ",top=" + (screen.innerHeight - 600) / 2);
 } 
 
 function download4() {
-    var novaJanela = window.open("form4.html", "_blank","width=505,height=1100,left=" + (screen.width - 800) / 2 + ",top=" + (screen.innerHeight - 600) / 2);
+    var novaJanela = window.open("https://lp.sebraeceara.com.br/e-book-5-passos-para-fazer-a-transformacao-digital", "_blank","width=505,height=1100,left=" + (screen.width - 800) / 2 + ",top=" + (screen.innerHeight - 600) / 2);
 } 
