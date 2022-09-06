f4885451d52ab7426fc04e34ae98b9e4248966b2
RaioDigital <raiodigitalweb@gmail.com>
2022-09-06 17:34:07 -0300

    Alteraçao dos links dos formularios dos baixaveis

--- assets/js/embed.js
+++ assets/js/embed.js
@@ -4,11 +4,11 @@ function download1() {
 } 
 
 function download2() {
-    var novaJanela = window.open("https://lp.sebraeceara.com.br/inforgrafico-salao-parceiro-o-que-e-e-como-funciona", "_blank","width=505,height=1100,left=" + (screen.width - 800) / 2 + ",top=" + (screen.innerHeight - 600) / 2);
+    var novaJanela = window.open("https://lp.sebraeceara.com.br/infografico-salao-parceiro-o-que-e-e-como-funciona", "_blank","width=505,height=1100,left=" + (screen.width - 800) / 2 + ",top=" + (screen.innerHeight - 600) / 2);
 } 
 
 function download3() {
-    var novaJanela = window.open("https://lp.sebraeceara.com.br/infografico-o-que-sao-canais-de-venda", "_blank","width=505,height=1100,left=" + (screen.width - 800) / 2 + ",top=" + (screen.innerHeight - 600) / 2);
+    var novaJanela = window.open("https://lp.sebraeceara.com.br/infografico-o-que-sao-canais-de-venda-e62948878916b7bc6172", "_blank","width=505,height=1100,left=" + (screen.width - 800) / 2 + ",top=" + (screen.innerHeight - 600) / 2);
 } 
 
 function download4() {
